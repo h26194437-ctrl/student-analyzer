c004d0e5da04028479f638fa328fc30fb3e99d04
h26194437-ctrl <h26194437@gmail.com>
2026-04-15 15:42:15 +0530

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,22 @@
+# constants.py
+
+GRADE_BOUNDARIES = {
+    "A+": 90,
+    "A": 80,
+    "B": 70,
+    "C": 60,
+    "D": 50,
+    "F": 0
+}
+
+DATA_FILE = "students.json"
+
+MENU_OPTIONS = [
+    "1. Add Student",
+    "2. Display All",
+    "3. Topper",
+    "0. Exit"
+]
+
+SEPARATOR = "-" * 40
+WELCOME_MESSAGE = "Student Grade Analyzer"
